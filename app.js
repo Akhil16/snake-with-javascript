3d25d7a53d8220b702e2e6695d0b039c9b678588
Akhil <akhilendu.chaturvedi@informa.com>
2022-10-29 13:44:51 +0530

    new logic

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentIndex = 0
     let snake = [2, 1, 0] // 2being head, 0 being the tail
     let direction = 1
+    let keyCode = 77777
     let score = 0
     let speed = 0.9
     let intervalTime = 0
@@ -23,76 +24,46 @@ document.addEventListener('DOMContentLoaded', () => {
             (snake[0] % width === 0 && direction === -1) ||
             blocks[snake[0] + direction].classList.contains('snake')
         ) {
-            alert("ouch! Space bar to restart");
-            console.log(score);
+            alert("ouch! Enter to restart");
             return clearInterval(interval)
         }
     }
 
     function checkBitingApple(tail) {
         //getting apple 
-        if (blocks[snake[0]].classList.contains('apple')) {
+        if (blocks[snake[0]] != undefined && blocks[snake[0]].classList.contains('apple')) {
             blocks[snake[0]].classList.remove('apple')
-            blocks[tail].classList.add('tail')
             snake.push(tail)
             randomApple()
             score++
             scoreSpan.textContent = score
             // clearInterval(interval)
             // intervalTime = intervalTime * speed
-            // interval = setInterval(moveOutcome, intervalTime)
+            // interval = setInterval(gameLoop, intervalTime)
         }
     }
 
     function moveSnake() {
         const tail = snake.pop() // popping tail
-        blocks[tail].classList.remove('tail')
         blocks[tail].classList.remove('snake')
+        blocks[tail].style = ''
         snake.unshift(snake[0] + direction) // moving head
         return tail;
     }
 
     function manageSkin() {
-        blocks[snake[0]].classList.add('snake')
-        blocks[snake[0]].classList.add('head')
-        blocks[snake[snake.length - 1]].classList.add('tail')
-        blocks[snake[1]].classList.remove('head')
-        blocks[snake[1]].classList.remove('rh')
-        blocks[snake[1]].classList.remove('lh')
-        blocks[snake[1]].classList.remove('uh')
-        blocks[snake[1]].classList.remove('dh')
-
-        headDirectionSkin()
-    }
-
-    function headDirectionSkin() {
-        switch (direction) {
-            case 1:
-                blocks[snake[0]].classList.add('rh')
-                blocks[snake[1]].classList.remove('rh')
-
-                break;
-            case -1:
-                blocks[snake[0]].classList.add('lh')
-                blocks[snake[1]].classList.remove('lh')
-
-                break;
-            case width:
-                blocks[snake[0]].classList.add('dh')
-                blocks[snake[1]].classList.remove('dh')
-
-                break;
-            case -width:
-                blocks[snake[0]].classList.add('uh')
-                blocks[snake[1]].classList.remove('uh')
-
-                break;
-
-            default:
-                break;
+        if (!!blocks[snake[0]]) {
+            blocks[snake[0]].classList.add('snake')
+            blocks[snake[0]].classList.add('head')
+            blocks[snake[1]].classList.remove('head')
+            snake.forEach((item, i) => {
+                if (i != 0 && i != snake.length - 1)
+                    blocks[item].style.scale = '0.' + Math.floor(Math.random() * 10)
+            })
         }
     }
-    function moveOutcome() {
+
+    function gameLoop() {
         checkBreath()
         const tail = moveSnake()
         checkBitingApple(tail)
@@ -107,60 +78,80 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     function start() {
         reset()
-        interval = setInterval(moveOutcome, intervalTime)
+        interval = setInterval(gameLoop, intervalTime)
     }
     function reset() {
         snake.forEach(index => {
+            if (!blocks[index]) return
             blocks[index].classList.remove('snake')
-            blocks[index].classList.remove('rh')
-            blocks[index].classList.remove('lh')
-            blocks[index].classList.remove('uh')
-            blocks[index].classList.remove('dh')
             blocks[index].classList.remove('head')
-            blocks[index].classList.remove('tail')
         })
+        blocks.forEach(item => item.style = '')
         blocks[appleIndex].classList.remove('apple')
         clearInterval(interval)
         direction = 1
         score = 0
         scoreSpan.textContent = score
-        intervalTime = 200
-        snake = [2, 1, 0]
+        intervalTime = 150
+        snake = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
         currentIndex = 0
         snake.forEach(index => blocks[index].classList.add('snake'))
         randomApple()
     }
     function controls(e) {
-        blocks[currentIndex].classList.remove('snake')
-        blocks[currentIndex].classList.remove('head')
-        blocks[currentIndex].classList.remove('tail')
+        if (keyCode === 37 && e.keyCode === 39 || keyCode === 38 && e.keyCode === 40 || keyCode === 39 && e.keyCode === 37 || keyCode === 40 && e.keyCode === 38) {
+            return;
+        }
+        if (keyCode === 65 && e.keyCode === 68 || keyCode === 87 && e.keyCode === 83 || keyCode === 68 && e.keyCode === 65 || keyCode === 83 && e.keyCode === 87) {
+            return;
+        }
         switch (e.keyCode) {
             case 37:
+                keyCode = e.keyCode
                 direction = -1
                 break
             case 38:
+                keyCode = e.keyCode
                 direction = - width
                 break
             case 39:
+                keyCode = e.keyCode
                 direction = 1
                 break
             case 40:
+                keyCode = e.keyCode
                 direction = width
                 break
-            case 32:
+            case 65:
+                keyCode = e.keyCode
+                direction = -1
+                break
+            case 87:
+                keyCode = e.keyCode
+                direction = - width
+                break
+            case 68:
+                keyCode = e.keyCode
+                direction = 1
+                break
+            case 83:
+                keyCode = e.keyCode
+                direction = width
+                break
+            case 13:
                 if (play) {
                     start()
                     play = false
-                    // console.log(1);
 
                 } else {
                     play = true
-                    // console.log(2);
-                    // reset()
+                    reset()
                     clearInterval(interval)
                 }
                 break
+
         }
+
     }
 
 
